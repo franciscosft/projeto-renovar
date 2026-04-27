bac74c287115093f871977a7b9001a7038f90723
Francisco Sacco Teixeira <franciscosft@gmail.com>
2026-04-27 11:51:13 +0100

    Migrate HomePage from Google Maps to Leaflet.js with OpenStreetMap tiles

--- frontend/src/pages/home/home.ts
+++ frontend/src/pages/home/home.ts
@@ -1,21 +1,30 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as L from 'leaflet';
 import { DeviceDTO } from '../../models/device.dto';
 import { DeviceService } from '../../services/domain/device.service';
 
-declare var google: any;
+// Fix broken default marker icons when bundled by Angular
+const iconDefault = L.icon({
+  iconUrl: 'assets/marker-icon.png',
+  iconRetinaUrl: 'assets/marker-icon-2x.png',
+  shadowUrl: 'assets/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: 'home.html',
-  imports: [
-  ],
+  imports: [],
   styleUrl: 'home.scss'
 })
 export class HomePage implements AfterViewInit {
   @ViewChild('map') mapElement!: ElementRef;
-  map: any;
 
   constructor(
     private router: Router,
@@ -23,29 +32,31 @@ export class HomePage implements AfterViewInit {
   ) {}
 
   ngAfterViewInit() {
+    const map = L.map(this.mapElement.nativeElement).setView(
+      [-27.6001426, -48.5182837],
+      13
+    );
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
     this.deviceService.findAll().subscribe({
-      next: response => this.initMap(response),
+      next: devices => this.addMarkers(map, devices),
       error: () => {}
     });
   }
 
-  initMap(devices: DeviceDTO[]) {
-    this.map = new google.maps.Map(this.mapElement.nativeElement, {
-      center: { lat: -27.6001426, lng: -48.5182837 },
-      zoom: 18,
-    });
-
+  private addMarkers(map: L.Map, devices: DeviceDTO[]) {
     devices.forEach(device => {
-      const marker = new google.maps.Marker({
-        position: {
-          lat: +device.coordinate.latitude,
-          lng: +device.coordinate.longitude
-        },
-        map: this.map,
-        title: device.name
-      });
+      const lat = +device.coordinate.latitude;
+      const lng = +device.coordinate.longitude;
 
-      marker.addListener('click', () => {
+      const marker = L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(device.name);
+
+      marker.on('click', () => {
         this.router.navigate(['/reading'], { state: { device } });
       });
     });
